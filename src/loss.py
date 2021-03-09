0adffab3278e010a285377d912bf7cbd3288a31e
Leo Heidelberger <leo.heidel@gmail.com>
2021-03-09 23:04:43 +0100

    Training working

--- src/loss.py
+++ src/loss.py
@@ -4,10 +4,10 @@ def N_tuple_loss(nb_patches, alpha=1, theta=0.35):
     @tf.function
     def loss(M, pred):
         diff = pred[:,:,tf.newaxis] - pred[:,tf.newaxis] # (N, nb_patches, nb_patches, features)
-        D = tf.norm(diff*diff, axis=-1) # (N, nb_patches, nb_patches)
+        D = tf.norm(diff*diff + 1e-10, axis=-1) # (N, nb_patches, nb_patches)
         norm_M = tf.reduce_sum(M, axis=[1,2])# (N,)
         loss_1 = (M*D) / norm_M[:, tf.newaxis, tf.newaxis]
-        loss_2 = tf.nn.relu(theta - (1-M)*D) / (nb_patches*nb_patches-norm_M)
-        return loss_1 * alpha*loss_2
+        loss_2 = tf.nn.relu(theta - (1-M)*D) / (nb_patches*nb_patches-norm_M)[:,tf.newaxis,tf.newaxis]
+        return loss_1 + alpha*loss_2
     return loss
     
