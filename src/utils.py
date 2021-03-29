fb96694d5c1afde9f71fb291fc078062cb21d23c
Leo Heidelberger <leo.heidel@gmail.com>
2021-03-29 15:46:13 +0200

    visualisation of prediction on cloud comapre

--- src/utils.py
+++ src/utils.py
@@ -6,11 +6,18 @@ def format_np_array(array):
     dtype=[('x', 'f4'), ('y', 'f4'),('z', 'f4')]
     return np.array([tuple(p) for p in array], dtype=dtype)
 
-def export_ply(file_name, vertices, normals=None):
+def format_np_array_scalar(array, name="scalar"):
+    dtype=[(name, 'f4')]
+    return np.array([(s,) for s in array], dtype=dtype)
+
+def export_ply(file_name, vertices, normals=None, scalar=None):
     points = PlyElement.describe(format_np_array(vertices), 'points')
     elements = [points]
     if normals is not None: 
         normals = PlyElement.describe(format_np_array(normals), 'normals')
-        elements.append(elements)
+        elements.append(normals)
+    if normals is not None: 
+        scalar = PlyElement.describe(format_np_array_scalar(scalar), 'scalar')
+        elements.append(scalar)
     
     PlyData(elements).write(file_name)
